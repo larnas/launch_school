710f4de0ef113f694973031361faf0e9e5bb7446
Panayiotis Larnakiotis <larnakiotispa@gmail.com>
2021-08-25 14:16:05 +0300

    folder renamed

---
+++ input_output/greeter.js
@@ -0,0 +1,5 @@
+let rlSync = require('readline-sync');
+let firstName = rlSync.question("What's your first name?");
+let lastName = rlSync.question("What's your last name?");
+
+console.log(`Hello, ${firstName} ${lastName}!`);
