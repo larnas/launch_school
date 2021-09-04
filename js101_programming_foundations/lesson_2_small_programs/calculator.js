594aed84abaf6e06ff2a5b0474c83fd90f04b22d
Panayiotis Larnakiotis <66648136+larnas@users.noreply.github.com>
2021-09-04 09:09:53 +0300

    refactoring

--- js101_programming_foundations/lesson_2_small_programs/calculator.js
+++ js101_programming_foundations/lesson_2_small_programs/calculator.js
@@ -4,28 +4,57 @@
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 
-const readline = require('readline-sync')
+const readline = require('readline-sync');
 
-console.log("wlcome to The Calculator!")
-
-console.log("What is the first number")
-let number1 = readline.question();
-
-console.log("What is the second number")
-let number2 = readline.question();
-
-console.log("What oparetion would you like to performe?\n1) Add 2) Subtract 3) Multiply 4) Divide")
-let operation = readline.question();
-
-let output;
-if (operation === '1') {
-  output = Number(number1) + Number(number2);
-} else if (operation === '2') {
-  output = Number(number1) - Number(number2)
-} else if (operation === '3') {
-  output = Number(number1) * Number(number2)
-} else if (operation === '4') {
-  output = Number(number1) / Number(number2)
+function prompt(msg) {
+  console.log(`=> ${msg}`);
 }
 
-console.log(`The result is ${output}`)
+function invalidNumber(num) {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+}
+
+prompt("welcome to The Calculator!");
+
+prompt("What is the first number");
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt("Hmmm...that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
+prompt("What is the second number");
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt("Hmmm...that doesn't look like a valid number.");
+  number2 = readline.question();
+}
+
+prompt("What oparetion would you like to performe?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+let operation = readline.question();
+
+while (!['1','2','3','4'].includes(operation)) {
+  prompt("Must choose 1, 2, 3 or 4.");
+  operation = readline.question();
+}
+
+let output;
+
+switch (operation) {
+  case '1':
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
+}
+
+prompt(`The result is ${output}`);
